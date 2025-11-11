fb3e6905fa9d8f099d1b7cb6df6a5240264d2a51
Ibrahim Ahmed Ibrahim <ibrahim.zagglol@gmail.com>
2025-11-11 13:46:05 +0200

    Update department.module.ts

--- src/modules/department/department.module.ts
+++ src/modules/department/department.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { DepartmentController } from './department.controller';
 import { DepartmentService } from './department.service';
 import { DepartmentRepository } from './repositories/department.repository';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DepartmentController],
   providers: [DepartmentService, DepartmentRepository],
-  exports: [DepartmentService],
+  exports: [DepartmentService, DepartmentRepository],
 })
 export class DepartmentModule {}
