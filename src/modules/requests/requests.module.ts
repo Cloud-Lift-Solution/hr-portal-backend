2c13d7dc16f9e934e3c82ef892e68180f39b4e17
Ibrahim Ahmed Ibrahim <ibrahim.zagglol@gmail.com>
2026-01-29 13:11:14 +0200

    Add models and services for new request types
    Extended the Prisma schema to include models and enums for vacation extension, vacation cancellation, sick leave, salary advance, and salary certificate requests. Updated the RequestsModule to register controllers and services for these new request types, enabling their management within the application.

--- src/modules/requests/requests.module.ts
+++ src/modules/requests/requests.module.ts
@@ -3,11 +3,42 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { JwtModule } from '../jwt/jwt.module';
 import { VacationRequestController } from './vacation-request/vacation-request.controller';
 import { VacationRequestService } from './vacation-request/vacation-request.service';
+import { SickLeaveRequestController } from './sick-leave-request/sick-leave-request.controller';
+import { SickLeaveRequestService } from './sick-leave-request/sick-leave-request.service';
+import { VacationExtensionRequestController } from './vacation-extension-request/vacation-extension-request.controller';
+import { VacationExtensionRequestService } from './vacation-extension-request/vacation-extension-request.service';
+import { VacationCancellationRequestController } from './vacation-cancellation-request/vacation-cancellation-request.controller';
+import { VacationCancellationRequestService } from './vacation-cancellation-request/vacation-cancellation-request.service';
+import { SalaryAdvanceRequestController } from './salary-advance-request/salary-advance-request.controller';
+import { SalaryAdvanceRequestService } from './salary-advance-request/salary-advance-request.service';
+import { SalaryCertificateRequestController } from './salary-certificate-request/salary-certificate-request.controller';
+import { SalaryCertificateRequestService } from './salary-certificate-request/salary-certificate-request.service';
 
 @Module({
   imports: [PrismaModule, JwtModule],
-  controllers: [VacationRequestController],
-  providers: [VacationRequestService],
-  exports: [VacationRequestService],
+  controllers: [
+    VacationRequestController,
+    SickLeaveRequestController,
+    VacationExtensionRequestController,
+    VacationCancellationRequestController,
+    SalaryAdvanceRequestController,
+    SalaryCertificateRequestController,
+  ],
+  providers: [
+    VacationRequestService,
+    SickLeaveRequestService,
+    VacationExtensionRequestService,
+    VacationCancellationRequestService,
+    SalaryAdvanceRequestService,
+    SalaryCertificateRequestService,
+  ],
+  exports: [
+    VacationRequestService,
+    SickLeaveRequestService,
+    VacationExtensionRequestService,
+    VacationCancellationRequestService,
+    SalaryAdvanceRequestService,
+    SalaryCertificateRequestService,
+  ],
 })
 export class RequestsModule {}
